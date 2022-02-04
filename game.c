81212eee1a860241ec3b0586163bf156c4e583f1
xuefeng <2106919811@qq.com>
2022-02-04 22:13:38 +0800

    budfix

--- game.c
+++ game.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main()
 {
-    int sj, N, s, count = 0;
+    int random_number, N, s, count = 0;
     srand((unsigned)time(NULL));
-    sj = rand() % 100 + 1; //sj为1~100之间的随机数
-    scanf("%d", &N);       //N表示最多猜测的次数
+    random_number = rand() % 100 + 1; // random_number为1~100之间的随机数
+    printf("猜数游戏开始！\n");
+    printf("请输入要猜的最大次数：");
+    scanf("%d", &N); // N表示最多猜测的次数
+    printf("请输入猜测的数字：\n");
+
     while (1)
     {
+        if (count++ >= N)
+        {
+            printf("\nGame Over");
+            printf("\n-----------------------\n");
+            count = 0;
+            break;
+        }
         scanf("%d", &s);
         getchar();
         if (s < 0)
         {
-            printf("Game Over"); //如果在到达N次之前，用户输入了一个负数，也输出"Game Over"，并结束程序
+            printf("\nGame Over"); //如果在到达N次之前，用户输入了一个负数，也输出"Game Over"，并结束程序
+            printf("\n-----------------------\n");
             count = 0;
             break;
         }
-        count++;
-        if (count > N)
-        {
-            printf("Game Over");
-            count = 0;
-            break;
-        }
-        if (s > sj)
+
+        if (s > random_number)
             printf("Too big\n");
-        else if (s < sj)
+        else if (s < random_number)
             printf("Too small\n");
         else
             break;
